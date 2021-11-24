8a4510482072f7c79350fb8e896ce1fc459fd9a1
Networkingskills <zenfox4it@hotmail.com>
2021-11-24 14:56:16 -0500

    Basic Config for router Loopback and routing process

---
+++ lib/Basic_config_script.py
@@ -0,0 +1,27 @@
+import getpass
+import telnetlib
+
+HOST = "Local Host"
+user = input("Enter your remote account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"config t\n")
+tn.write(b"inter loopback 1\n")
+tn.write(b"ip add 1.1.1.1 255.255.255.255\n")
+tn.write(b"exit\n")
+tn.write(b"router ospf 1\n")
+tn.write(b"network 0.0.0.0 255.255.255.255 area 0\n")
+tn.write(b"exit\n")
+tn.write(b"end\n")
+tn.write(b"wr\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
